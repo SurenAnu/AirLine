5ed0b55ba9b4e998706c4e4e69604b689b3f615a
SurenAnu <kuttisurendhar2013@gmail.com>
2023-05-25 16:05:14 +0530

    Air Line Project

---
+++ src/main/java/org/AirLine_Project/Croatia_Airlines/App.java
@@ -0,0 +1,13 @@
+package org.AirLine_Project.Croatia_Airlines;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
